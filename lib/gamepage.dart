cac04c9e5f2e9c1c6403c80ee5409e6f1b798eb7
Adnan Azem <adnan.azem.250@gmail.com>
2023-08-22 19:27:18 +0300

    improve

--- lib/gamepage.dart
+++ lib/gamepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:async';
 
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
@@ -7,7 +9,70 @@ class GamePage extends StatefulWidget {
   State<GamePage> createState() => _GamePageState();
 }
 
+Icon getRandomArrow() {
+  Random random = Random();
+  int choice = random.nextInt(2);
+
+  // Set the arrow icon based on the chosen choice
+  IconData iconData = choice == 0 ? Icons.arrow_back : Icons.arrow_forward;
+  Icon res = Icon(
+    iconData,
+    size: 50,
+    color: getRandomRedOrGreenColor(),
+  );
+  return res;
+}
+
+Color getRandomRedOrGreenColor() {
+  Random random = Random();
+
+  // Generate a random number (0 or 1) to choose between red and green
+  int choice = random.nextInt(2);
+
+  // Set the color component values based on the chosen color
+  int red = choice == 0 ? 255 : 0;
+  int green = choice == 1 ? 255 : 0;
+  int blue = 0; // Set blue to 0 for a pure red or green color
+
+  // Create a Color object using the chosen color components
+  Color color = Color.fromARGB(255, red, green, blue);
+  return color;
+}
+
+String getRandomPhoto() {
+  final List<String> photoAssets = [
+    'asserts/images/arrows2.png',
+    'asserts/images/rainbow2.jpeg'
+  ];
+
+  Random random = Random();
+  int randomIndex = random.nextInt(photoAssets.length);
+  return photoAssets[randomIndex];
+}
+
 class _GamePageState extends State<GamePage> {
+  bool isButtonPressed = false;
+  Timer? longPressTimer;
+  DateTime? startTime;
+  Icon randomArrow = getRandomArrow();
+  String randomPhoto = getRandomPhoto();
+
+  void cancelTimer() {
+    longPressTimer?.cancel();
+  }
+
+  void startTimer() {
+    startTime = DateTime.now();
+    longPressTimer = Timer(Duration(seconds: 10), () {
+      setState(() {
+        cancelTimer();
+        DateTime endTime = DateTime.now();
+        Duration responseTime = endTime.difference(startTime!);
+        print('Response time: ${responseTime.inMilliseconds} milliseconds');
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +82,14 @@ class _GamePageState extends State<GamePage> {
             flex: 1,
             child: Center(
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    print("red");
+                    startTimer();
+                    randomArrow = getRandomArrow();
+                    randomPhoto = getRandomPhoto();
+                  });
+                },
                 child: Text(""),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.red,
@@ -31,13 +103,45 @@ class _GamePageState extends State<GamePage> {
             flex: 6,
             child: Column(
               children: [
-                Expanded(flex: 3, child: Center(child: Icon(Icons.arrow_forward, size: 50, color: Colors.white),),),
-                Expanded(child: Center(child: Text(""),),),
-                Expanded(flex: 3, child: Center(child: Icon(Icons.arrow_back, size: 50, color: Colors.white),),),
-                
-                  // child: Icon(Icons.arrow_back, size: 50, color: Colors.white),
-                  // color: Colors.black,
-                
+                Expanded(
+                  child: GestureDetector(
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(randomPhoto),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: randomArrow,
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      onLongPress: () {
+                        setState(() {
+                          print("Hold");
+                          startTimer();
+                          isButtonPressed = true;
+                        });
+                      },
+                      // isButtonPressed = false,
+                      child: Text("keep holding!"),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: isButtonPressed?Colors.purple: Colors.grey,
+                        padding: EdgeInsets.all(100),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
@@ -46,7 +150,14 @@ class _GamePageState extends State<GamePage> {
             // color: Colors.black,
             child: Center(
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    print("green");
+                    startTimer();
+                    randomArrow = getRandomArrow();
+                    randomPhoto = getRandomPhoto();
+                  });
+                },
                 child: Text(""),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green,
